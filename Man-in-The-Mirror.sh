a139fa97d84dbe383972cca87f603381eed1c80f
Tacoquito <115024123+Tacoquito271828@users.noreply.github.com>
2024-09-11 19:06:54 -0500

    Se añade un script para configurar IPtables

---
+++ Man-in-The-Mirror.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Usage "./Man-in-The-Mirror.sh [Interface] [PUERTO/S; /Separados por coma; 0 para puertos default]"
+
+ip=$(ifconfig $1 | grep -i "inet" | grep -v inet6|  awk '{print $2}')
+
+if [ "$2" -eq "0" ]; then sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $ip:80
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination $ip:443
+
+
+
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+exit 1
+fi
+
+im=$(echo $2 | sed 's/,/\n/g' | wc -l)
+
+i=1
+
+while [ "$i" -le "$im" ]; do sudo iptables -t nat -A PREROUTING -p tcp --dport $(echo $2 | awk -F ',' -v i=$i '{print $i}') -j DNAT --to-destination $ip:$(echo $2 | awk -F ',' -v i=$i '{print $i}'); i=$(($i+1)); done && sudo iptables -t nat -A POSTROUTING -j MASQUERADE
